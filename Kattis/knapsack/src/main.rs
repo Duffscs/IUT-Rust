d72f4d0965cace05f204d801a254b8313c58fe32
Gauthier Raphael <raphael.gauthier@etu.unilim.fr>
2021-03-24 20:59:09 +0100

    fonction

--- Kattis/knapsack/src/main.rs
+++ Kattis/knapsack/src/main.rs
@@ -16,42 +16,8 @@ fn main() {
             vec.push(a);
         }
 
-        let mut vm: Vec<Vec<usize>> = vec![vec![0; c + 1]; n + 1];
-        let mut vm_pos: Vec<Vec<(Option<usize>, Option<usize>)>> = vec![vec![(None, None); c + 1]; n + 1];
+        let vec = sac_a_dos(c, n, &mut vec);
 
-        for i in 1..=n {
-            for j in 1..=c {
-                let (vi, mi) = vec[i - 1];
-                if mi > j {
-                    vm[i][j] = vm[i - 1][j];
-                    vm_pos[i][j] = vm_pos[i - 1][j]
-                } else {
-                    let prev_vm = vm[i - 1][j];
-                    let next_vm = vm[i - 1][j - mi] + vi;
-                    if prev_vm >= next_vm {
-                        vm[i][j] = prev_vm;
-                        vm_pos[i][j] = vm_pos[i - 1][j]
-                    } else {
-                        vm[i][j] = next_vm;
-                        vm_pos[i][j] = (Some(i - 1), Some(j - mi));
-                    }
-                }
-            }
-        }
-
-        // vm.iter().for_each(|e| println!("{:?}", e));
-        // println!();
-        //
-        // vm_pos.iter().for_each(|e| println!("{:?}", e));
-        // println!();
-
-        let mut vec: Vec<usize> = Vec::new();
-
-        let mut i = vm_pos[n][c];
-        while i.0 != None && i.1 != None {
-            vec.push(i.0.unwrap());
-            i = vm_pos[i.0.unwrap()][i.1.unwrap()];
-        }
         if vec.len() == 0 {
             println!("0");
         }else {
@@ -60,8 +26,40 @@ fn main() {
             println!();
         }
 
-
-
         bool = word.clone().count() != 0;
     }
+}
+
+fn sac_a_dos(c: usize, n: usize, vec: &mut Vec<(usize, usize)>) -> Vec<usize> {
+    let mut vm: Vec<Vec<usize>> = vec![vec![0; c + 1]; n + 1];
+    let mut vm_pos: Vec<Vec<(Option<usize>, Option<usize>)>> = vec![vec![(None, None); c + 1]; n + 1];
+
+    for i in 1..=n {
+        for j in 1..=c {
+            let (vi, mi) = vec[i - 1];
+            if mi > j {
+                vm[i][j] = vm[i - 1][j];
+                vm_pos[i][j] = vm_pos[i - 1][j]
+            } else {
+                let prev_vm = vm[i - 1][j];
+                let next_vm = vm[i - 1][j - mi] + vi;
+                if prev_vm >= next_vm {
+                    vm[i][j] = prev_vm;
+                    vm_pos[i][j] = vm_pos[i - 1][j]
+                } else {
+                    vm[i][j] = next_vm;
+                    vm_pos[i][j] = (Some(i - 1), Some(j - mi));
+                }
+            }
+        }
+    }
+
+    let mut vec: Vec<usize> = Vec::new();
+
+    let mut i = vm_pos[n][c];
+    while i.0 != None && i.1 != None {
+        vec.push(i.0.unwrap());
+        i = vm_pos[i.0.unwrap()][i.1.unwrap()];
+    }
+    vec
 }
